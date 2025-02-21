fb3c621ccdfbdf7f9fffb308e3b6a7e33b69b757
Ethan Hoglund <60147729+ethnhglnd101@users.noreply.github.com>
2025-02-21 13:43:31 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,20 @@ document.addEventListener("keydown", function(event) {
     if (event.key === "Enter") {
         let contentDiv = document.getElementById("content");
         let newParagraph = document.createElement("p");
-        newParagraph.textContent = "Here's more text!";
+        newParagraph.textContent = "Ymj Rtzxj nx qnxjxynts. Ymj Hfy nx Uwtbqnlwf.";
         contentDiv.appendChild(newParagraph);
+    
     }
 });
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        let contentDiv = document.getElementById("content");
+        let newParagraph = document.createElement("p");
+        newParagraph.textContent = "Wbxrl tkabfldpkq sbjglnp slkk rlst ylt tge pdklqn vgo bkmlf hfql mbx fjmd a bjfls.";
+        contentDiv.appendChild(newParagraph);
+    
+    }
+});
+
+
